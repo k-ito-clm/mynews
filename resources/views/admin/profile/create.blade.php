ed3fd1898df09f710f3bbd64961b70bbaf666b37
”35_ito_kaori” <k_ito@columbusman.co.jp>
2020-10-27 02:53:39 +0000

    Laravel 12 ユーザー認証

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<!--<html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,6 +8,23 @@
         <title>create</title>
     </head>
     <body>
-        <h1>create ページ</h1>
+        <h1>課題提出ページ</h1>
     </body>
-</html>
+</html>-->
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'課題'を埋め込む --}}
+@section('title', '課題')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>課題提出画面</h2>
+            </div>
+        </div>
+    </div>
+@endsection
