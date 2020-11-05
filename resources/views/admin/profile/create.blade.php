c7c1e726b2b1e57ff844c6c797558899c154a478
”35_ito_kaori” <k_ito@columbusman.co.jp>
2020-11-05 02:49:16 +0000

    PHP/Laravel 14課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -25,7 +25,15 @@
             <div class="col-md-8 mx-auto">
                 <h2>課題提出画面</h2>
                  <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
-
+ 
+                        @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                        
                 <div class="form-group row">
                     <label class="col-md-2">氏名</label>
                     <div class="col-md-10">
@@ -50,7 +58,9 @@
                             <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
                         </div>
                     </div>
-               
+                {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
