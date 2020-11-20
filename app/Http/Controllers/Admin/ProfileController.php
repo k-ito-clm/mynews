71529778267524394c05bce57d421acaff824d6e
”35_ito_kaori” <k_ito@columbusman.co.jp>
2020-11-20 03:10:53 +0000

    PHP/Laravel 16課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -26,12 +26,32 @@ class ProfileController extends Controller
       
         return redirect('admin/profile/create');
     }
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
+   
+    public function edit(Request $request)
+  {
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
+      return view('admin.profile.edit', ['profile_form' => $profile]);
+  }
+    
+     public function update(Request $request)
+  {
+      // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // News Modelからデータを取得する
+      $profile = Profiel::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      
+      unset($profile_form['remove']);
+      unset($profile_form['_token']);
+
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+
+      return redirect('admin/profile');
+  }
+    
 }
