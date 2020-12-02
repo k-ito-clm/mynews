f57bba1e6c943f3abbbfda68cd630fda898fae56
”35_ito_kaori” <k_ito@columbusman.co.jp>
2020-12-02 14:55:07 +0000

    profile

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -29,6 +29,19 @@ class ProfileController extends Controller
         return redirect('admin/profile/create');
     }
    
+   public function index(Request $request)
+  {
+      $cond_name = $request->cond_name;
+      if ($cond_name != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('name', $cond_name)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
+  }
+   
     public function edit(Request $request)
   {
       // News Modelからデータを取得する
